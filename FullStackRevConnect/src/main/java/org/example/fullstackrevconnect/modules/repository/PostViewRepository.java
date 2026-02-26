124466297e64a4abb621b6578ec402889fb2176a
sandhiya <sandhya1220sandy@gmail.com>
2026-02-26 15:04:57 +0530

    Developed post ,comment,feed module implementation

--- FullStackRevConnect/src/main/java/org/example/fullstackrevconnect/modules/repository/PostViewRepository.java
+++ FullStackRevConnect/src/main/java/org/example/fullstackrevconnect/modules/repository/PostViewRepository.java
@@ -1,4 +1,16 @@
 package org.example.fullstackrevconnect.modules.repository;
 
-public interface PostViewRepository {
+import org.example.fullstackrevconnect.modules.entity.PostView;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PostViewRepository extends JpaRepository<PostView, Long> {
+
+    List<PostView> findByPostId(String postId);
+
+    List<PostView> findByPostIdAndViewedAtAfter(String postId, LocalDateTime date);
+
+    boolean existsByPostIdAndUserId(String postId, Long userId);
 }
